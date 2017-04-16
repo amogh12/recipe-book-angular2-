4ff4250c79aad9b0fab30c964f54a2a341a83a40
ASP <amogh.gates@gmail.com>
2017-04-16 16:26:39 -0400

    adding the form controls

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,20 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { Ingredient } from '../shared/ingredient';
-import { ShoppingListService } from './shopping-list.service';
+import { ShoppingListAddComponent } from "./shopping-list-add.component";
 
+import { ShoppingListService } from "./shopping-list.service";
+import { Ingredient } from '../shared/ingredient';
 
 @Component({
   selector: 'rb-shopping-list',
-  templateUrl: './shopping-list.component.html'
+  templateUrl: 'shopping-list.component.html'
 })
 export class ShoppingListComponent implements OnInit {
-
   items: Ingredient[] = [];
+  selectedItem: Ingredient = null;
 
-  constructor(private sls: ShoppingListService) { }
+  constructor(private sls: ShoppingListService) {}
 
   ngOnInit() {
     this.items = this.sls.getItems();
   }
 
+  onSelectItem(item: Ingredient) {
+    this.selectedItem = item;
+  }
+
+  onCleared() {
+    this.selectedItem = null;
+  }
 }
